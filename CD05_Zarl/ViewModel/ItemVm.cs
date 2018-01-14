6c9db8227d7dd88889943105910d089eb9d7764b
Matthias Zarl <matthias.zarl@gmail.com>
2018-01-14 16:48:16 +0100

    Add project files.

---
+++ CD05_Zarl/ViewModel/ItemVm.cs
@@ -0,0 +1,46 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+
+namespace CD05_Zarl.ViewModel
+{
+    public class ItemVm : ViewModelBase
+    {
+        public string Description { get; set; }
+        public BitmapImage Image { get; set; }
+        public ObservableCollection<ItemVm> Items { get; set; }
+        public string AgeRecommendation { get; set; }
+
+        public int NoOfParts
+        {
+            get
+            {
+                if (Items != null)
+                {
+                    return Items.Count;
+                }
+                else return 0;
+
+            }
+        }
+
+
+        public ItemVm(string description, BitmapImage image, string age)
+        {
+            Description = description;
+            Image = image;
+            this.AgeRecommendation = age;
+        }
+
+        public void AddItem(ItemVm item)
+        {
+            if (Items == null)
+            { 
+                Items = new ObservableCollection<ItemVm>();
+            }
+
+            Items.Add(item);
+        }
+
+    }
+}
